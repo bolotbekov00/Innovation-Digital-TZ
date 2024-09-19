b5f396784a98a18155059a41c4a6255364b88a13
bolotbekov00 <bolotbekov0606gmail.com>
2024-09-19 15:45:27 +0600

    feat: new project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0
+  const handelClick = () => {
+    count++
+  }
+</script>
+
+<h1>Hello {count}</h1>
+<button on:click={handelClick}>increment</button>
+
+
+<style>
+
+</style>
